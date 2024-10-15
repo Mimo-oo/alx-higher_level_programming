48f50196738f96c183fd119ad04c3f3f79595113
mimo-oo <fadipemichael6@gmail.com>
2024-10-15 14:46:30 -0700

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,5 @@
-#!/bin/bash/python3
-
+#!/usr/bin/python3
 """A function that writes a string to a text file"""
-
 
 
 def write_file(filename="", text=""):
@@ -9,10 +7,7 @@ def write_file(filename="", text=""):
     """
         Writing into a file
     """
+    with open(filename, 'w', encoding="utf-8") as f:
+        write_data = f.write(text)
 
-
-
-    with open(filename, "w", encoding="utf-8") as f:
-        write_text = f.write(text)
-
-    return write_text
+    return write_data
