65e1da544d1f71e187a834a2567a829a8fd04862
mimo-oo <fadipemichael6@gmail.com>
2024-10-15 14:51:44 -0700

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,9 @@
-#!/bin/bash/python3
-
-"""to_json_string module"""
+#!/usr/bin/python3
+"""``to_json_string`` module"""
 
 import json
 
+
 def to_json_string(my_obj):
-
     """Returns the JSON representation of an object (string)"""
-
     return json.dumps(my_obj)
