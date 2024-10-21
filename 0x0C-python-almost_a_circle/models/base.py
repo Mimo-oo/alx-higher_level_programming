4e252144e6d56869cdff5bc9301ada48b100a3bf
Mimo-oo <fadipemichael6@gmail.com>
2024-10-21 14:22:26 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-
 """
 Defines a base model class.
 """
-
+import json
+import csv
+import turtle
 
 class Base:
-
     """
     Represents the base model
     """
-
     __nb_objects = 0
-
     def __init__(self, id=None):
         if id is not None:
-            self.id = int(id)
-
+            self.id = id
         else:
-           Base.__nb_objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
