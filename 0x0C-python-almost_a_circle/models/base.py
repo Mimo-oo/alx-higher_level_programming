558ce7f667548f2e844d41c699d21acc8f9084c7
Mimo-oo <fadipemichael6@gmail.com>
2024-10-22 05:22:10 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
+
 """
 Defines a base model class.
 """
 
+
 class Base:
+
     """
     Represents the base model
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
-        Initialization of the class 
+        Initialization of the class
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns JSON representation of list_dictionaries
+        """
+        if list_dictionaries = None or list_dictionaries = {}:
+            return "[]"
+        else:
+            with open(list_dictionaries, "r", encoding"utf-8") as f:
+                f.dumps(list_dictionaries)
