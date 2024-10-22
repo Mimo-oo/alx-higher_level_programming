b1a30cc1a7e9285d49a6ed6f305ea450c904d92b
Mimo-oo <fadipemichael6@gmail.com>
2024-10-22 08:10:09 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,11 @@ class Base:
                 for obj in list_objs:
                     list_dict.append(obj.to_dictionary())
                 f.write(Base.to_json_string(list_dict))
+
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string
+        """
+        if json_string is None or json_string == []:
+            return "[]"
+        return json_string
