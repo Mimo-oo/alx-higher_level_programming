bcbd62a7d897c3c79f2b6c02013bbc814157387b
mimo-oo <fadipemichael6@gmail.com>
2024-11-20 15:50:33 -0800

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creates another table in a database
+CREATE TABLE second_table(
+	id = 1, name = “John”, score = 10;
+	id = 2, name = “Alex”, score = 3;
+	id = 3, name = “Bob”, score = 14;
+	id = 4, name = “George”, score = 8;
+	);
