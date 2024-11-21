96df54d0912199145b13483caae5fdefa18d4caf
mimo-oo <fadipemichael6@gmail.com>
2024-11-21 15:19:40 -0800

    6 states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates table with constraints
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `states`(
+		id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+		name VARCHAR(256)
+		);
