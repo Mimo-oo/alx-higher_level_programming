408b61f40b74e436ae99620f5c1b7cc6c80aa48e
Mimo-oo <fadipemichael6@gmail.com>
2024-12-16 13:36:34 -0800

    muti language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints 3 lines
+
+
+let c = 'C is fun';
+let python = 'Python is cool';
+let javascript = 'Javascript is amazing';
+
+console.log(c);
+console.log(python);
+console.log(javascript);
