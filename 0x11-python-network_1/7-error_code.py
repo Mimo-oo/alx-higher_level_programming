e52688f1ff501739809d64cbfb0ee57103e92ea8
mimo-oo <fadipemichael6@gmail.com>
2025-01-21 23:49:43 +0100

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python Script that takes in a URL, sends a request to the URL
+& displays the body of the response
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    response = requests.get(argv[1])
+    status = response.status_code
+    print(response.text) if status < 400 else print(
+            "Error code: {}".format(r.status_code))
