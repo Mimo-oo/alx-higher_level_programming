3c85e904677df0158a8c0c748af740432bc0f37e
mimo-oo <fadipemichael6@gmail.com>
2025-01-21 23:36:05 +0100

    error code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Script that takes in a URL and an email, sends a POST request
  & displays the body of the response
- """
+"""
 
-import sys
-import urllib.parse
-import urllib.request
 
 if __name__ == "__main__":
+    import sys
+    import urllib.parse
+    import urllib.request
+
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
